c4e59763657bbacd6a60aeb715bc5ddd896310c9
Ryli Dunlap <ryli@transvec.com>
2024-07-05 10:46:59 -0500

    Added an endpoint to list orders associated with a tag and tests

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,10 +1,11 @@
-
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
+from interview.order.views import (OrderListCreateView, OrderTagListCreateView,
+    OrdersForOrderTagListView)
 
 
 urlpatterns = [
-    path('tags/', OrderTagListCreateView.as_view(), name='order-detail'),
+    path('tags/', OrderTagListCreateView.as_view(), name='order-tags-list'),
+    path('tags/<int:id>/orders/', OrdersForOrderTagListView.as_view(), name='order-for-tags-list'),
     path('', OrderListCreateView.as_view(), name='order-list'),
 
 ]
